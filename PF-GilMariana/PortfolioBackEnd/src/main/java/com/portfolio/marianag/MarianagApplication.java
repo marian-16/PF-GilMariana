fd122fbe601a12193e41bab4fc32153d517a300e
marian-16 <118771335+marian-16@users.noreply.github.com>
2023-05-07 18:35:20 -0300

    Instalacion Java

---
+++ PF-GilMariana/PortfolioBackEnd/src/main/java/com/portfolio/marianag/MarianagApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.marianag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarianagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarianagApplication.class, args);
+	}
+
+}
---
+++ PF-GilMariana/PortfolioBackEnd/src/test/java/com/portfolio/marianag/MarianagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.marianag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarianagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
